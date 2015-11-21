56fcf340f4e22d37ef63e1717105bd0a6775df5e
miamidougwarner <dougcanjam@gmail.com>
2015-11-21 12:40:02 -0600

    Filled in matrix caching operations. Added a test function for time display

--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,68 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
-
+    aMatrix <- NULL
+    
+    # save the original version of the matrix
+    set <- function(inMatrix) {
+        x <<- inMatrix
+        aMatrix <<- NULL
+    }
+  
+    get <- function() x
+    setinverse <- function(solve) aMatrix <<- solve
+    getinverse <- function() aMatrix
+    list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned 
+## by makeCacheMatrix above. If the inverse has already been calculated 
+## (and the matrix has not changed), then the cachesolve should retrieve 
+## the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+
+    aMatrix <- x$getinverse()
+    
+    
+    if(!is.null(aMatrix)) {
+          # The inverse has already been computed. Just return it.
+          
+          #message("retrieving cached inverse")
+          return(aMatrix)
+      }
+    
+
+    theData <- x$get()
+
+    # Compute the matrix inverse
+    aMatrix <- solve(theData,...)
+    
+    x$setinverse(aMatrix)
+  
+  	aMatrix
+}
+
+# Test function for the cached matrix problem. The default matrix is a 750x750 
+# matrix. When this funtion runs, it creates a special cached matrix. Then it shows
+# the time to create the inverse and then it shows the time to retrieve the inverse
+# from the cache. (This is interesting with matrices of 1m or more elements.)
+cacheTest <- function(inSquareInvertibleMatrix = matrix(rnorm(562500),nrow=750,ncol=750)) {
+    
+    aCacheMatrix <- makeCacheMatrix( inSquareInvertibleMatrix )
+    
+    startTime <- Sys.time()
+    cacheSolve(aCacheMatrix)
+    anInterval1 <- Sys.time() - startTime
+    startTime2 <- Sys.time()
+    cacheSolve(aCacheMatrix)
+    anInterval2 <- Sys.time() - startTime2
+    
+    print(paste("Interval 1", anInterval1, sep=" "))
+    print(paste("Interval 2", anInterval2, sep=" "))
 }
